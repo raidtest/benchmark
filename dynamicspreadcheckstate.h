9b4cad5b5e3ccee1dae08af474e74563b7fda421
windtai <feng.tai@thomsonreuters.com>
2023-10-29 22:40:18 +0800

    align name rule.

--- dynamicspreadcheckstate.h
+++ dynamicspreadcheckstate.h
@@ -20,13 +20,13 @@ namespace ben_tol_lib
     class spread_exception_table
     {
     public:
-        void updateContCntAfterDeleteEntry(const std::string &i_delEntryContName);
-        void updateContCntAfterAddEntry(const std::string &i_addEntryContName);
-        void delEntry(const std::string &i_delEntryContName, const bool i_isOldest, spread_avg_param &o_delparam);
-        unsigned int getDiffContNums();
-        void clearContCnt();
-        void clearContCntAndETList();
-        int getContNums(const std::string &i_cont);
+        void update_cont_cnt_after_delete_entry(const std::string &i_delEntryContName);
+        void update_cont_cnt_after_add_entry(const std::string &i_addEntryContName);
+        void del_entry(const std::string &i_delEntryContName, const bool i_isOldest, spread_avg_param &o_delparam);
+        unsigned int get_diff_cont_nums();
+        void clear_cont_cnt();
+        void clear_cont_cnt_and_ET_list();
+        int get_cont_nums(const std::string &i_cont);
     public:
         std::list<std::pair<std::string, spread_avg_param>> etList_; //contributor,spread
     private:
@@ -48,7 +48,7 @@ namespace ben_tol_lib
         {
             STATIC_LIMIT_TYPE,
             PERCENT_LIMIT_TYPE,
-        } SreadCheckTypeEnum;
+        } sread_check_type_enum;
         typedef struct
         {
             double vSAvg_; //Spread Average from all spreads(regardless passed or failed).
@@ -76,34 +76,34 @@ namespace ben_tol_lib
             bool vLMMAccept_;
         } MemberObservorStruct;
         dynamic_spreadcheck_state(const double i_spreadLimit, log_tool *i_logTool = 0);
-        bool getETLogInformation(std::string &o_ETinfo);
-        double getCurrentDynSpreadLimit();
-        void setCurrentDynSpreadLimit(const double i_spreadLimit);
-        void setStatisticInfo(ben_uint64 i_vSMMAccepted, ben_uint64 i_vSMMRejected, ben_uint64 i_vLMMAccepted, ben_uint64 i_vLMMRejected);
-        void clearStatisticInfo();
-        void clearState();  //when receive any configure changed in dynamic spread check, reuse it for
-        bool getAllMemberValue(MemberObservorStruct &o_struct); //serialize,show all the state to VAD
-        void recordCheckResult(SreadCheckTypeEnum i_type, const double i_currentSpread, const std::string &i_contributor, ben_uint32 i_spreadDate, ben_uint32 i_spreadTime, bool i_isPass, const CSpreadSetting &i_SpreadSetting);//record the spread check result
+        bool get_ET_log_information(std::string &o_ETinfo);
+        double get_current_dynspread_limit();
+        void set_current_dynspread_limit(const double i_spreadLimit);
+        void set_statistic_info(ben_uint64 i_vSMMAccepted, ben_uint64 i_vSMMRejected, ben_uint64 i_vLMMAccepted, ben_uint64 i_vLMMRejected);
+        void clear_statistic_info();
+        void clear_state();  //when receive any configure changed in dynamic spread check, reuse it for
+        bool get_all_value(MemberObservorStruct &o_struct); //serialize,show all the state to VAD
+        void record_check_result(sread_check_type_enum i_type, const double i_currentSpread, const std::string &i_contributor, ben_uint32 i_spreadDate, ben_uint32 i_spreadTime, bool i_isPass, const CSpreadSetting &i_SpreadSetting);//record the spread check result
 
         //
         //note we rename the isTriggerDynamicSpreadCheck to GetDynamicSpreadCheckLimit for more readable.  May 9,2016
         //the return bool meaning: if the limit is changed or not.
         //
-        bool GetDynamicSpreadCheckLimit(SreadCheckTypeEnum/*i_type*/, const double i_currentSpread, const std::string &i_contributor, const CSpreadSetting &i_SpreadSetting, const boost::posix_time::ptime &i_currentTime);
+        bool get_dynamic_spread_check_limit(sread_check_type_enum/*i_type*/, const double i_currentSpread, const std::string &i_contributor, const CSpreadSetting &i_SpreadSetting, const boost::posix_time::ptime &i_currentTime);
         //        bool isTriggerDynamicSpreadCheck(SreadCheckTypeEnum/*i_type*/, const double i_currentSpread, const std::string &i_contributor, const CSpreadSetting &i_SpreadSetting, const boost::posix_time::ptime& i_currentTime);
     private:
-        bool isTriggeredAfterCheckTableET(const double i_currentSpread, const CSpreadSetting &i_spreadSetting, const boost::posix_time::ptime &i_currentTime);
-        bool getSpreadLimitAfterCheck(const double i_configStaticSpreadLimit, const double i_smallMoveMultiplier, const double i_largeMoveMutiplier, const double i_maxSmallMoveStepWise, const double i_maxLargeMoveStepWise, const double i_maxPassPct, const ben_uint64 i_minContSM, const ben_uint64 i_maxContLM, bool i_isMMRejectedAlert = false, bool i_isMMAcceptAlert = false);
+        bool is_triggered_after_check_table_ET(const double i_currentSpread, const CSpreadSetting &i_spreadSetting, const boost::posix_time::ptime &i_currentTime);
+        bool get_spread_limit_after_check(const double i_configStaticSpreadLimit, const double i_smallMoveMultiplier, const double i_largeMoveMutiplier, const double i_maxSmallMoveStepWise, const double i_maxLargeMoveStepWise, const double i_maxPassPct, const ben_uint64 i_minContSM, const ben_uint64 i_maxContLM, bool i_isMMRejectedAlert = false, bool i_isMMAcceptAlert = false);
     private:
-        void clearHistoryInputs();
-        void updateInput(const double i_currentStaOrPctSpread, const CSpreadSetting &i_spreadSetting, const std::string &i_cont);
+        void clear_history_inputs();
+        void update_input(const double i_currentStaOrPctSpread, const CSpreadSetting &i_spreadSetting, const std::string &i_cont);
     private:
-        bool calAlgorithmVariables(const double /*i_configStaticSpreadLimit*/, const double i_smallMoveMultiplier, const double i_largeMoveMutiplier, const double i_maxSmallMoveStepWise, const double i_maxLargeMoveStepWise, const ben_uint64 i_minContSM, const ben_uint64 i_maxContLM, const double/*i_maxPassPct*/);
-        void checkSMSLAndLMSLValidWhenGreaterCurrentSpreadLimt(std::string *o_errorInfo = nullptr);
-        void checkSMSLValidWhenInterveneStaticLimitAndCurrent(const double i_maxPassPct);
-        void checkSMSLValidWhenLessStaticLimit(const double i_configStaticLimit, const double i_maxPassPct);
-        void checkSMSLInOtherConditon();
-        void checkLMSLValid();
+        bool cal_algorithm_variables(const double /*i_configStaticSpreadLimit*/, const double i_smallMoveMultiplier, const double i_largeMoveMutiplier, const double i_maxSmallMoveStepWise, const double i_maxLargeMoveStepWise, const ben_uint64 i_minContSM, const ben_uint64 i_maxContLM, const double/*i_maxPassPct*/);
+        void check_SMSL_and_LMSL_valid_when_greater_current_spread_limt(std::string *o_errorInfo = nullptr);
+        void check_SMSL_valid_when_intervene_static_limit_and_current(const double i_maxPassPct);
+        void check_SMSL_valid_when_less_static_limit(const double i_configStaticLimit, const double i_maxPassPct);
+        void check_SMSL_in_other_conditon();
+        void check_LMSL_valid();
     private:
     public: //for non-intrusive boost serialization. temporary solution, Wind.
         const static int MAX_AVERAGE_SPREAD_NEED_SPREAD_NUMS;
